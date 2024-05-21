761c89112231b07cec7c76a7d87bfcf6d616fc58
Kamsi Kodi <kamsikodi@gmail.com>
2024-05-21 09:23:14 +0100

    first commit

--- src/routes/Products.jsx
+++ src/routes/Products.jsx
@@ -1,7 +1,30 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const ProductsPage = () => {
     const navigate = useNavigate();
+    const [colleges, setColleges] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const options = {
+                method: 'GET',
+                url: 'http://universities.hipolabs.com/search?country=United+States',
+                headers: {
+                  'content-type': 'application/json'
+                }
+            };
+              
+            try {
+                const response = await axios.request(options);
+                setColleges(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+    }, []);
 
     const goHomeHandler = () => {
        navigate('/');
@@ -10,6 +33,15 @@ const ProductsPage = () => {
         <div>
             Products Page
             <p onClick={goHomeHandler} style={{textDecoration: 'underline' }}>Go home</p>
+
+            <h2>Univeristy List</h2>
+            {
+                colleges.map((college, index) => index < 10 && (
+                        <div key={college.name}>
+                            <p>Name: <a href={college.web_pages} target="__blank">{college.name}</a></p>
+                        </div>
+                ))
+            }
         </div>
     );
 }
